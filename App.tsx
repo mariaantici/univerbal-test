5146911f6f8cd73ed81c6ab89d051d3e220873c0
mariaantici <maria.antici99@gmail.com>
2025-01-09 14:54:49 +0100

    general: display icons for bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import TopRatedScreen from '@/screens/top-rated';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { appRouteNames } from '@/routes';
 import { z } from 'zod';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -29,17 +30,32 @@ export default function App() {
         <Tab.Screen
           name="tab-home"
           component={HomeScreen}
-          options={{ tabBarLabel: () => null }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" color={color} size={size} />
+            ),
+          }}
         />
         <Tab.Screen
           name="tab-top-rated"
           component={TopRatedScreen}
-          options={{ tabBarLabel: () => null }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="star" color={color} size={size} />
+            ),
+          }}
         />
         <Tab.Screen
           name="tab-favorites"
           component={FavoritesScreen}
-          options={{ tabBarLabel: () => null }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="favorite" color={color} size={size} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
