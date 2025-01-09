514eb04f325e82a670247e4bf64240f873b1d3ac
mariaantici <maria.antici99@gmail.com>
2025-01-09 17:50:51 +0100

    movie and tv-series: display all data

--- app/screens/movie/index.tsx
+++ app/screens/movie/index.tsx
@@ -1,13 +1,26 @@
 import { View, Text } from 'react-native';
+import { Movie } from '@/../domain/movie';
 
 type MovieScreenProps = {
-  route: any;
+  route: {
+    params: {
+      movie: Movie; // Pass the entire movie object as a route parameter
+    };
+  };
 };
 
-export default function MovieScreen(props: MovieScreenProps) {
+export default function MovieScreen({ route }: MovieScreenProps): JSX.Element {
+  const { movie } = route.params;
+
   return (
     <View>
-      <Text>favorite movie: {props.route.id}</Text>
+      <Text>ID: {movie.id}</Text>
+      <Text>Title: {movie.title}</Text>
+      <Text>Director: {movie.director}</Text>
+      <Text>Release Year: {movie.releaseYear}</Text>
+      <Text>Genres: {movie.genres.join(', ')}</Text>
+      <Text>Rating: {movie.rating}</Text>
+      <Text>Runtime: {movie.runtimeMinutes} minutes</Text>
     </View>
   );
 }
--- app/screens/tv-series/index.tsx
+++ app/screens/tv-series/index.tsx
@@ -1,10 +1,36 @@
-import { ReactNode } from 'react';
 import { View, Text } from 'react-native';
+import { TVSeries } from '@/../domain/tv-series';
 
-export default function TvSeriesScreen(): ReactNode {
+type TvSeriesScreenProps = {
+  route: {
+    params: {
+      tvSeries: TVSeries; // Pass the entire TV series object as a route parameter
+    };
+  };
+};
+
+export default function TvSeriesScreen({
+  route,
+}: TvSeriesScreenProps): JSX.Element {
+  const { tvSeries } = route.params;
+
   return (
     <View>
-      <Text>favorite tv series</Text>
+      <Text>ID: {tvSeries.id}</Text>
+      <Text>Title: {tvSeries.title}</Text>
+      <Text>Creator: {tvSeries.creator}</Text>
+      <Text>Release Year: {tvSeries.releaseYear}</Text>
+      <Text>Genres: {tvSeries.genres.join(', ')}</Text>
+      <Text>Rating: {tvSeries.rating}</Text>
+      <Text>Seasons: {tvSeries.seasons.length}</Text>
+      {tvSeries.seasons.map((season, index) => (
+        <View key={season.seasonId}>
+          <Text>
+            Season {index + 1} ({season.releaseYear})
+          </Text>
+          <Text>Episodes: {season.episodes.length}</Text>
+        </View>
+      ))}
     </View>
   );
 }
